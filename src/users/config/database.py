816213b0cfb216ac825396aa1a9d2a985cce8a38
lilvolcanointhenighttime <eliotalderson031@gmail.com>
2023-11-05 23:37:07 +0300

    Making product and productcategory

--- src/users/config/database.py
+++ src/users/config/database.py
@@ -4,7 +4,7 @@ import os
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["DB_NAME_STORE_MANAGEMENT"],
+        "NAME": os.environ["DB_NAME_USERS"],
         "USER": os.environ["DB_USER"],
         "PASSWORD": os.environ["DB_PASSWORD"],
         "HOST": os.environ["DB_HOST"],
